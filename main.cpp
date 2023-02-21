7cf2d13c70be44a0cb6aa8fb15b0291b2fbf6a73
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2023-02-21 11:21:14 -0500

    Update main.cpp
    Actualización de ejemplo de herencia en Cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 //#include <string>
 using namespace std;
 class Vampiro{
+public:
+    Vampiro(string nombre, int edad) : nombre(nombre), edad(edad) {}
+
 private:
     string nombre;
     int edad;
@@ -15,10 +18,23 @@ public:
 
 };
 
-class Dracula : public Vampiro{};
+class Dracula : public Vampiro{
+
+private:
+    int estatura;
+public:
+    //Dracula(string nombre, int edad, int estatura):nombre(nombre), edad(edad) ,estatura(estatura){};
+    Dracula(string nombre,int edad, int estatura):Vampiro(nombre, edad),estatura(estatura){
+        cout<< "Se ha creado un objeto Dracula de nombre "<<nombre<<" de edad: "<<edad <<" y con estatura: "<<estatura<<endl;
+
+
+    }; /* Ejecución del constructor de la clase base para inicializar a unaVar */
+    //ClaseDerivada(int x):ClaseBase(x){} /* Ejecución del constructor de la clase base para inicializar a unaVar */
+};
 
 int main() {
-    Dracula *obj1=new Dracula();
+
+    Dracula *obj1=new Dracula("Vladimir",200,185);
 
     obj1->morder();
     obj1->chuparSangre();
